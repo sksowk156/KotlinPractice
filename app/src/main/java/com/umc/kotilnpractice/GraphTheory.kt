3a4f2b8bdc6ba1773cf77944ed1700f306d99aba
jinee <sksowk156@gmail.com>
2023-04-03 22:53:25 +0900

    20230403

---
+++ app/src/main/java/com/umc/kotilnpractice/GraphTheory.kt
@@ -0,0 +1,271 @@
+package com.umc.kotilnpractice
+
+import kotlin.math.cos
+
+//fun main(){
+//    fun find_parent(parent: MutableList<Int>, x: Int): Int {
+//        if (parent[x] != x) {
+//            parent[x] = find_parent(parent, parent[x])
+//        }
+//        return parent[x]
+//    }
+//
+//    fun union_parent(parent: MutableList<Int>, a: Int, b: Int) {
+//        val a = find_parent(parent, a)
+//        val b = find_parent(parent, b)
+//        if (a < b) { // 더 작은 값이 상대방의 부모가 된다.
+//            parent[b] = a
+//        } else {
+//            parent[a] = b
+//        }
+//    }
+//
+//    // 노드 개수, 간선 개수 입력 받기
+//    val (v, e) = readln().split(" ").map { it.toInt() }
+//    val parent = MutableList<Int>(v+1){0}
+//
+//    // 부모 테이블상에서 부모를 자기 자신으로 초기화
+//    for(i in 1..v){
+//        parent[i] = i
+//    }
+//
+//    var cycle = false
+//
+//    // union 집합 만들기
+//    repeat(e){
+//        val (a,b)= readln().split(" ").map { it.toInt() }
+//
+//        if(find_parent(parent,a)==find_parent(parent,b)){
+//            cycle = true
+//            return@repeat
+//        }else{
+//            union_parent(parent, a, b)
+//        }
+//    }
+//
+//    if(cycle){
+//        println("사이클이 발생")
+//    }else{
+//        println("사이클이 발생하지 않음")
+//        // 부모 테이블 만들기
+//        println("각 원소가 속한 집합 : ")
+//        for(i in 1.. v){
+//            print(find_parent(parent,i).toString()+" ")
+//        }
+//
+//        println()
+//
+//        // 부모 테이블 출력
+//        println("부모 테이블 : ")
+//        for(i in 1..v){
+//            print(parent[i].toString()+" ")
+//        }
+//    }
+//}
+
+//fun main(){
+//    fun find_parent(parent : MutableList<Int>, x : Int) : Int{
+//        if(parent[x] != x){
+//            parent[x] = find_parent(parent, parent[x])
+//        }
+//        return parent[x]
+//    }
+//
+//    fun union_parent(parent:MutableList<Int>, a:Int, b:Int){
+//        val a = find_parent(parent,a)
+//        val b = find_parent(parent,b)
+//        if(a < b){
+//            parent[b] = a
+//        }else{
+//            parent[a] = b
+//        }
+//    }
+//
+//    val (v,e)= readln().split(" ").map{it.toInt()}
+//    val parent = MutableList<Int>(v+1){0}
+//
+//    val edges = mutableListOf<Triple<Int,Int,Int>>()
+//    var result = 0
+//
+//    for(i in 1..v){
+//        parent[i]=i
+//    }
+//
+//    repeat(e){
+//        val (a,b,cost) = readln().split(" ").map { it.toInt() }
+//        edges.add(Triple(cost,a,b))
+//    }
+//
+//    edges.sortBy { it.first }
+//
+//    for(edge in edges){
+//        val (cost, a, b) = edge
+//        if(find_parent(parent, a)!=find_parent(parent,b)){
+//            union_parent(parent,a,b)
+//            result += cost
+//        }
+//    }
+//    print(result)
+//}
+
+//fun main(){
+//    val (v,e) = readln().split(" ").map { it.toInt() }
+//    val indegree = MutableList<Int>(v+1){0}
+//    val graph = Array(v+1){
+//        MutableList<Int>(v+1){0}
+//    }
+//
+//    repeat(e){
+//        val(a,b) = readln().split(" ").map { it.toInt() }
+//        graph[a].add(b)
+//        indegree[b]++
+//    }
+//
+//    fun topology_sort(){
+//        val result = mutableListOf<Int>()
+//        val q = ArrayDeque<Int>()
+//
+//        for(i in 1..v){
+//            if(indegree[i]==0){
+//                q.addLast(i)
+//            }
+//        }
+//
+//        while(q.isNotEmpty()){
+//            val now = q.removeFirst()
+//            result.add(now)
+//            for(i in graph[now]){
+//                indegree[i]--
+//                if(indegree[i]==0){
+//                    q.addLast(i)
+//                }
+//            }
+//        }
+//
+//        for(i in result){
+//            print("$i ")
+//        }
+//    }
+//
+//    topology_sort()
+//}
+
+//// 2
+//fun main() {
+//    fun find_parent(array: MutableList<Int>, a: Int): Int {
+//        if(array[a]!=a){
+//            array[a] = find_parent(array,array[a])
+//        }
+//        return array[a]
+//    }
+//
+//    fun union(array: MutableList<Int>, a: Int, b: Int) {
+//        val a = find_parent(array,a)
+//        val b = find_parent(array,b)
+//        if(a>b){
+//            array[a] = b
+//        }else{
+//            array[b] = a
+//        }
+//    }
+//
+//    val (N,M) = readln().split(" ").map{it.toInt()}
+//    val array = MutableList(N+1){0}
+//
+//    for(i in 0..N){
+//        array[i] = i
+//    }
+//
+//    val graph = mutableListOf<Triple<Int,Int,Int>>()
+//    repeat(M){
+//        val(a,b,c) = readln().split(" ").map { it.toInt() }
+//        graph.add(Triple(a,b,c))
+//    }
+//
+//    for(i in graph){
+//        if(i.first==0){
+//            union(array,i.second,i.third)
+//        }else{
+//            if(find_parent(array,i.second) == find_parent(array,i.third)){
+//                println("YES")
+//            }else{
+//                println("NO")
+//            }
+//        }
+//    }
+//}
+
+//// 3
+//fun main() {
+//    fun find_parent(parent: MutableList<Int>, a: Int): Int {
+//        if (parent[a] != a) {
+//            parent[a] = find_parent(parent, parent[a])
+//        }
+//        return parent[a]
+//    }
+//
+//    fun union(parent: MutableList<Int>, a: Int, b: Int) {
+//        val a = find_parent(parent, a)
+//        val b = find_parent(parent, b)
+//
+//        if (a < b) {
+//            parent[b] = a
+//        } else {
+//            parent[a] = b
+//        }
+//
+//    }
+//
+//    val (N, M) = readln().split(" ").map { it.toInt() }
+//    val parent = MutableList<Int>(N + 1) { 0 }
+//
+//    for (i in 1..N) {
+//        parent[i] = i
+//    }
+//
+//    val edge = MutableList<Triple<Int, Int, Int>>(M) { Triple(0, 0, 0) }
+//    var result = 0
+//
+//    repeat(M) {
+//        val (a, b, c) = readln().split(" ").map { it.toInt() }
+//        edge.add(Triple(a, b, c))
+//    }
+//
+//    edge.sortBy { it.third }
+//
+//    var last = 0
+//    for(i in edge){
+//        val (a,b,c) = i
+//        if(find_parent(parent,a)!=find_parent(parent,b)){
+//            union(parent,a,b)
+//            result+=c
+//            last = c
+//        }
+//    }
+//
+//    println(result-last)
+//}
+
+// 4
+fun main(){
+    val n = readln().toInt()
+    val indegree = MutableList<Int>(n+1){0}
+
+    val graph = Array(n+1){
+        MutableList<Int>(n+1){0}
+    }
+
+    repeat(n){
+        val temp = readln().split(" ").map { it.toInt() }.toMutableList()
+        if(temp.size > 2){
+            for(i in 2..temp.size-2){
+                graph[it].add(temp[i])
+            }
+        }
+    }
+
+    for(i in array){
+
+    }
+
+}
--- app/src/main/java/com/umc/kotilnpractice/ShortestRoute.kt
+++ app/src/main/java/com/umc/kotilnpractice/ShortestRoute.kt
@@ -32,7 +32,7 @@ import java.util.PriorityQueue
 //                val cost = dist + i.second // 연결된 노드까지의 거리 계산
 //                if(cost<distance[i.first]){ // 최단 거리 정보에서 연결된 노드까지 거리와 위에서 계산한 거리 비교
 //                    distance[i.first] = cost // cost가 더 작으면 갱신
-//                    q.add(Pair(cost, i.first)) // 우선 순위 큐에 일단 넣음(알아서 최소값을 맨 앞으로 옮겨준다.)
+//                    q.add(Pair(cost, i.first)) // 갱신된 노드만 우선 순위 큐에 넣는다.(알아서 최소값을 맨 앞으로 옮겨준다.)
 //                }
 //            }
 //        }
@@ -92,116 +92,164 @@ import java.util.PriorityQueue
 //
 //}
 
-// 3
-fun main() {
-//    // N : 노드 개수, M : 간선 개수
-//    val (N, M) = readln().split(" ").map { it.toInt() }
-//    // 노드 간 경로
-//    val array = Array(M) {
-//        readln().split(" ").map { it.toInt() }.toMutableList() // 서로 연결된 노드 a, b
+//// 3
+//fun main() {
+////    // N : 노드 개수, M : 간선 개수
+////    val (N, M) = readln().split(" ").map { it.toInt() }
+////    // 노드 간 경로
+////    val array = Array(M) {
+////        readln().split(" ").map { it.toInt() }.toMutableList() // 서로 연결된 노드 a, b
+////    }
+////    // X : 목표 노드, K : 중간 노드
+////    val (X, K) = readln().split(" ").map { it.toInt() }
+////
+////    // 최단 거리를 저장할 배열
+////    var temp = Array(100) { 1000000 }
+////
+////    // 방문 배열
+////    var visit = Array(N + 1) { 0 }
+////
+////    fun dijkstra(start: Int) {
+////        // 한 노드에 연결된 다른 노드들을 저장할 배열
+////        val temp2 = mutableListOf<Int>()
+////        temp[0] = 0
+////        temp[start] = 0
+////        visit[start] = 1
+////        for (i in array) {
+////            if (i[0] == start) {
+////                temp[i[1]] = 1
+////                temp2.add(i[1])
+////                visit[i[1]] = 1
+////            } else if (i[1] == start) {
+////                temp[i[0]] = 1
+////                temp2.add(i[0])
+////                visit[i[0]] = 1
+////            }
+////        }
+////
+////        while (temp2.isNotEmpty()) {
+////            val node = temp2.removeAt(0)
+////            for (i in array) {
+////                if (i[0] == node && visit[i[1]] == 0) {
+////                    temp[i[1]] = kotlin.math.min(temp[node] + 1, temp[i[1]])
+////                    temp2.add(i[1])
+////                    visit[i[1]] = 1
+////                } else if (i[1] == node && visit[i[0]] == 0) {
+////                    temp[i[0]] = kotlin.math.min(temp[node] + 1, temp[i[0]])
+////                    temp2.add(i[0])
+////                    visit[i[0]] = 1
+////                }
+////            }
+////        }
+////    }
+////
+////    dijkstra(1)
+////    var x = 0
+////    x = if (temp[K] == 1000000) {
+////        -1
+////    } else {
+////        temp[K]
+////    }
+////
+////    temp = Array(100) { 1000000 }
+////    visit = Array(N + 1) { 0 }
+////    dijkstra(K)
+////    var k = if (temp[X] == 1000000) {
+////        -1
+////    } else {
+////        temp[X]
+////    }
+////
+////    if (x > 0 && k > 0) {
+////        print(x + k)
+////    } else {
+////        print(-1)
+////    }
+//
+////    val INF = 1e9.toInt()
+////
+////    val (n,m) = readln().split(" ").map { it.toInt() }
+////    var graph = Array(n+1){
+////        MutableList(n+1){INF}
+////    }
+////
+////    for(a in 1..n){
+////        for(b in 1.. n){
+////            if(a==b){
+////                graph[a][b] = 0
+////            }
+////        }
+////    }
+////
+////    repeat(m){
+////        val (a,b) = readln().split(" ").map { it.toInt() }
+////        graph[a][b] = 1
+////        graph[b][a] = 1
+////    }
+////
+////    val (x,k) = readln().split(" ").map { it.toInt() }
+////
+////    for(k in 1..n){
+////        for( a in 1..n){
+////            for(b in 1..n){
+////                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+////            }
+////        }
+////    }
+////
+////    val distance = graph[1][k] + graph[k][x]
+////
+////    if( distance >= INF){
+////        print("-1")
+////    }else{
+////        print(distance)
+////    }
+//}
+
+//// 4
+//const val INF = 1e9.toInt()
+//fun main(){
+//    val (N, M, C) = readln().split(" ").map{it.toInt() }
+//    val graph = Array(N+1){
+//        MutableList<Pair<Int,Int>>(N+1){Pair(0,0)}
 //    }
-//    // X : 목표 노드, K : 중간 노드
-//    val (X, K) = readln().split(" ").map { it.toInt() }
+//    val array = MutableList(N+1){INF}
 //
-//    // 최단 거리를 저장할 배열
-//    var temp = Array(100) { 1000000 }
+//    repeat(M){
+//        val (X, Y, Z) = readln().split(" ").map { it.toInt() }
+//        graph[X].add(Pair(Y,Z))
+//    }
 //
-//    // 방문 배열
-//    var visit = Array(N + 1) { 0 }
+//    fun dijkstra(start:Int){
+//        array[start] = 0
+//        val q = PriorityQueue<Pair<Int,Int>>(){a,b->a.second-b.second}
 //
-//    fun dijkstra(start: Int) {
-//        // 한 노드에 연결된 다른 노드들을 저장할 배열
-//        val temp2 = mutableListOf<Int>()
-//        temp[0] = 0
-//        temp[start] = 0
-//        visit[start] = 1
-//        for (i in array) {
-//            if (i[0] == start) {
-//                temp[i[1]] = 1
-//                temp2.add(i[1])
-//                visit[i[1]] = 1
-//            } else if (i[1] == start) {
-//                temp[i[0]] = 1
-//                temp2.add(i[0])
-//                visit[i[0]] = 1
+//        q.add(Pair(start, 0))
+//
+//        while(q.isNotEmpty()){
+//            val (a,b) = q.poll()!!
+//            if(array[a] < b){
+//                continue
 //            }
-//        }
-//
-//        while (temp2.isNotEmpty()) {
-//            val node = temp2.removeAt(0)
-//            for (i in array) {
-//                if (i[0] == node && visit[i[1]] == 0) {
-//                    temp[i[1]] = kotlin.math.min(temp[node] + 1, temp[i[1]])
-//                    temp2.add(i[1])
-//                    visit[i[1]] = 1
-//                } else if (i[1] == node && visit[i[0]] == 0) {
-//                    temp[i[0]] = kotlin.math.min(temp[node] + 1, temp[i[0]])
-//                    temp2.add(i[0])
-//                    visit[i[0]] = 1
+//            for(i in graph[a]){
+//                if(array[i.first] > b+i.second){
+//                    array[i.first] = b+i.second
+//                    q.add(Pair(i.first, b+i.second))
 //                }
 //            }
 //        }
 //    }
 //
-//    dijkstra(1)
-//    var x = 0
-//    x = if (temp[K] == 1000000) {
-//        -1
-//    } else {
-//        temp[K]
-//    }
-//
-//    temp = Array(100) { 1000000 }
-//    visit = Array(N + 1) { 0 }
-//    dijkstra(K)
-//    var k = if (temp[X] == 1000000) {
-//        -1
-//    } else {
-//        temp[X]
-//    }
-//
-//    if (x > 0 && k > 0) {
-//        print(x + k)
-//    } else {
-//        print(-1)
-//    }
-
-//    val INF = 1e9.toInt()
-//
-//    val (n,m) = readln().split(" ").map { it.toInt() }
-//    var graph = Array(n+1){
-//        MutableList(n+1){INF}
-//    }
-//
-//    for(a in 1..n){
-//        for(b in 1.. n){
-//            if(a==b){
-//                graph[a][b] = 0
-//            }
+//    dijkstra(C)
+//    var count = 0
+//    for(i in array.indices){
+//        if(array[i]>=INF){
+//            println("${i}로 가는 길 없음")
+//        }else if(array[i] > 0){
+//            println("${i}로 가는 길 있음")
+//            count++
 //        }
 //    }
 //
-//    repeat(m){
-//        val (a,b) = readln().split(" ").map { it.toInt() }
-//        graph[a][b] = 1
-//        graph[b][a] = 1
-//    }
-//
-//    val (x,k) = readln().split(" ").map { it.toInt() }
-//
-//    for(k in 1..n){
-//        for( a in 1..n){
-//            for(b in 1..n){
-//                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
-//            }
-//        }
-//    }
-//
-//    val distance = graph[1][k] + graph[k][x]
-//
-//    if( distance >= INF){
-//        print("-1")
-//    }else{
-//        print(distance)
-//    }
-}
+//    println(count.toString()+ " "+ array.max())
+//}
